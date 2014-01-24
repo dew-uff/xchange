51120f755f4eef0b2e5d0299e3a631851a0ace00
Jorge Moreira <jorge.smrr@gmail.com>
2014-01-24 12:42:30 -0200

    Melhorias na criação de condições para regras

--- src/GUI/Rules/LineRule.java
+++ src/GUI/Rules/LineRule.java
@@ -1,6 +1,6 @@
 package GUI.Rules;
 
-
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
@@ -14,21 +14,21 @@ import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-    /**
-     * @author Celio Henrique Nogueira Larcher Júnior
-     * Classe contém cada linha em que a regra é montada, inicializando seus combobox e seus eventos.
-     */
-public class LineRule extends JPanel{  
+/**
+ * @author Celio Henrique Nogueira Larcher Júnior 
+ * Classe contém cada linha em que a regra é montada, inicializando seus combobox e seus eventos.
+ */
+public class LineRule extends JPanel {
+
     private JComboBox comboTerm1, comboTerm2, comboOperator;
     private JButton btnAddCondition;
     private JButton btnRemoveCondition;
     private static JPanel pnlRules;
-    private static ArrayList<LineRule> linerules;
     private static String[] namesFacts;
 
-    LineRule(){
+    LineRule() {
         super(new FlowLayout());
-        this.setMaximumSize(new Dimension(730,32));
+        this.setMaximumSize(new Dimension(730, 32));
         this.comboTerm1 = new JComboBox();
         this.comboTerm2 = new JComboBox();
         this.comboOperator = new JComboBox();
@@ -41,64 +41,74 @@ public class LineRule extends JPanel{
         this.add(btnRemoveCondition);
         this.btnAddCondition.addActionListener(new ActionListener() {
             /**
-             * evento do botão adicionar condição
+             * evento do botão de adicionar condição
              */
             public void actionPerformed(ActionEvent e) {
-                 LineRule aux=new LineRule();
-                 linerules.add(aux);
-                 pnlRules.add(aux);
-                 btnAddCondition.setEnabled(false);
-                 aux.comboTerm1.requestFocus();
-                 pnlRules.revalidate();
+                LineRule aux = new LineRule();
+                pnlRules.add(aux);
+                btnAddCondition.setEnabled(false);
+                ((LineRule) pnlRules.getComponent(0)).btnRemoveCondition.setEnabled(true);
+                aux.comboTerm1.requestFocus();
+                pnlRules.revalidate();
             }
         });
-       final LineRule aux=this;
-       this.btnRemoveCondition.addActionListener(new ActionListener() {
+        final LineRule aux = this; //utilizada pra os Listeners
+        this.btnRemoveCondition.addActionListener(new ActionListener() {
+            /**
+             * evento do botão de remover condição
+             */
             public void actionPerformed(ActionEvent e) {
-                if(linerules.size()>1){
-                 int i=linerules.indexOf(aux);
-                 if(i==linerules.size()-1){
-                     linerules.get(i-1).btnAddCondition.setEnabled(true);
-                      pnlRules.updateUI();
-                 }
-                 linerules.remove(aux);
-                 pnlRules.remove(aux);
-                }else JOptionPane.showMessageDialog(pnlRules,"It's necessary one or more conditions to create a rule.");
-                linerules.get(linerules.size()-1).btnAddCondition.requestFocus();
+                int size = pnlRules.getComponentCount();
+                if (size > 1) {
+                    if (pnlRules.getComponent(size - 1) == aux) {
+                        ((LineRule) pnlRules.getComponent(size - 2)).btnAddCondition.setEnabled(true);
+                    }
+                    pnlRules.remove(aux);
+                    if (size == 2) {
+                        ((LineRule) pnlRules.getComponent(0)).btnRemoveCondition.setEnabled(false);
+                    }
+                    pnlRules.updateUI();
+                } else {
+                    JOptionPane.showMessageDialog(pnlRules, "It's necessary one or more conditions to create a rule.");
+                }
+                ((LineRule) pnlRules.getComponent(size - 1)).btnAddCondition.requestFocus();
                 pnlRules.revalidate();
             }
         });
         this.comboOperator.addItemListener(new ItemListener() {
-         public void itemStateChanged(ItemEvent e) {
-             if((comboOperator.getSelectedItem().toString().indexOf("_"))>=0){ //somente os operadores de diferença tem esse simbolo ex.: element_deleted
-                if(linerules.indexOf(aux)==0){
-                 comboTerm1.setEnabled(false);
-                 comboTerm2.setEnabled(false);
-                 comboTerm1.setSelectedItem("");
-                 comboTerm2.setSelectedItem("");
-                 btnAddCondition.setEnabled(false);
-               }else{
-                    JOptionPane.showMessageDialog(pnlRules,"The new_element operator and the element_deleted operator do not support more than 1 condition.");
-                    comboOperator.setSelectedItem("");
-               }
-             }else{
-                 comboTerm1.setEnabled(true);
-                 comboTerm2.setEnabled(true);
-                 btnAddCondition.setEnabled(true);
-             }
-        }
+            public void itemStateChanged(ItemEvent e) {
+                if(e.getStateChange() == ItemEvent.SELECTED){
+                    if ((comboOperator.getSelectedItem().toString().indexOf("_")) >= 0) { //somente os operadores de diferença tem esse simbolo ex.: element_deleted
+                        if (pnlRules.getComponentCount() == 1) { //só permite selecionar operadores de diferença se a regra tiver somente 1 condição
+                            comboTerm1.setEnabled(false);
+                            comboTerm2.setEnabled(false);
+                            comboTerm1.setSelectedItem("");
+                            comboTerm2.setSelectedItem("");
+                            btnAddCondition.setEnabled(false);
+                        } else {
+                            JOptionPane.showMessageDialog(pnlRules, "The new_element operator and the element_deleted operator do not support more than 1 condition.");
+                            comboOperator.setSelectedItem("");
+                        }
+                    } else {
+                        comboTerm1.setEnabled(true);
+                        comboTerm2.setEnabled(true);
+                        if (pnlRules.getComponent(pnlRules.getComponentCount() - 1) == aux) { //só ativa o botão "+" na última condição
+                            btnAddCondition.setEnabled(true);
+                        }
+                    }
+                }
+            }
         });
         this.comboTerm1.setModel(new DefaultComboBoxModel(getDoubleNames(namesFacts)));
         this.comboTerm2.setModel(new DefaultComboBoxModel(getDoubleNames(namesFacts)));
-        this.comboTerm1.insertItemAt("",0);
-        this.comboTerm2.insertItemAt("",0);
+        this.comboTerm1.insertItemAt("", 0);
+        this.comboTerm2.insertItemAt("", 0);
         this.comboTerm2.setSelectedItem("");
         this.comboTerm1.setSelectedItem("");
-        this.comboOperator.setModel(new DefaultComboBoxModel(new String[] {"",">", "<", "=","!=","new_element","deleted_element"}));
+        this.comboOperator.setModel(new DefaultComboBoxModel(new String[]{"", ">", "<", "=", "!=", "new_element", "deleted_element"}));
         this.comboOperator.setSelectedItem("");
     }
-            
-            
+
     public JComboBox getComboTerm1() {
         return comboTerm1;
     }
@@ -106,36 +116,35 @@ public class LineRule extends JPanel{
     public JComboBox getComboTerm2() {
         return comboTerm2;
     }
-    
+
     public JComboBox getComboOperator() {
         return comboOperator;
     }
-    
+
     public JButton getBtnAddCondition() {
         return btnAddCondition;
     }
-    
-    
-     /**
-     * Monta as opções dos comboBox de termos para a construção das regras. 
+
+    /**
+     * Monta as opções dos comboBox de termos para a construção das regras.
      * Exemplo: (fato - v. Before / fato - v. After).
      * @param namesFacts
      * Vetors de Strings contendo o nome de todos os fatos.
      * @return doubleNames
      * Nomes duplificados e normalizados.
-     */   
-    private String[]  getDoubleNames(String[] namesFacts){
+     */
+    private String[] getDoubleNames(String[] namesFacts) {
         String[] doubleNames;
         ArrayList<String> listNomesDuplicados = new ArrayList<String>();
-       
-        for (int i=0; i< namesFacts.length; i++) {
-               listNomesDuplicados.add(namesFacts[i]+" - v. Before");
-               listNomesDuplicados.add(namesFacts[i]+" - v. After");
+
+        for (int i = 0; i < namesFacts.length; i++) {
+            listNomesDuplicados.add(namesFacts[i] + " - v. Before");
+            listNomesDuplicados.add(namesFacts[i] + " - v. After");
         }
-        doubleNames = (String[]) listNomesDuplicados.toArray(new String[listNomesDuplicados.size()]);         
+        doubleNames = (String[]) listNomesDuplicados.toArray(new String[listNomesDuplicados.size()]);
         return doubleNames;
     }
-    
+
     public static JPanel getPnlRules() {
         return pnlRules;
     }
@@ -145,11 +154,22 @@ public class LineRule extends JPanel{
     }
 
     public static ArrayList<LineRule> getLinerules() {
-        return linerules;
+        ArrayList<LineRule> lineRules = new ArrayList<LineRule>();
+        for (Component c : pnlRules.getComponents()) {
+            lineRules.add((LineRule) c);
+        }
+        return lineRules;
     }
 
     public static void setLinerules(ArrayList<LineRule> linerules) {
-        LineRule.linerules = linerules;
+        if (pnlRules != null) {
+            pnlRules.removeAll();
+            for (LineRule lineRule : linerules) {
+                pnlRules.add(lineRule);
+            }
+            pnlRules.updateUI();
+            pnlRules.revalidate();
+        }
     }
 
     public static String[] getNamesFacts() {
@@ -159,6 +179,4 @@ public class LineRule extends JPanel{
     public static void setNamesFacts(String[] namesFacts) {
         LineRule.namesFacts = namesFacts;
     }
-
-
 }
