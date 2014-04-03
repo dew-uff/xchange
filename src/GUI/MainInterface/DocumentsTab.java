90d27edc4344c534bda4f9539a9f2da9ab18cd06
Jorge Moreira <jorge.smrr@gmail.com>
2014-04-03 09:45:29 -0300

    Melhoria na interface de Semantic Diff

--- src/GUI/MainInterface/DocumentsTab.java
+++ src/GUI/MainInterface/DocumentsTab.java
@@ -1,21 +1,16 @@
 package GUI.MainInterface;
 
-import Documents.Document;
 import Documents.Documents;
 import GUI.Layout.LayoutConstraints;
 import Manager.Manager;
-import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import javax.swing.BoxLayout;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 
 /**
  *
@@ -32,9 +27,6 @@ public class DocumentsTab extends JPanel implements ActionListener {
     private Documents documents;
     private Manager manager;
     private MainInterface main;
-    private ArrayList<LineFile> lineFiles;
-    private JPanel pnlFiles;
-    private final static int MOVE_UP = 1, REMOVE = 0, MOVE_DOWN = -1; //utilizadas para controlar a lista de documentos
 
     /**
      * Construtor da classe.
@@ -49,36 +41,6 @@ public class DocumentsTab extends JPanel implements ActionListener {
         GridBagLayout gridBag = new GridBagLayout();
         GridBagConstraints constraints = new GridBagConstraints();
         this.setLayout(gridBag);
-
-        //cria o painel para a lista de documentos abertos
-        JPanel pnlFilesList = new JPanel();
-        setMinimumSize(new Dimension(310, 40));
-        pnlFilesList.setBorder(javax.swing.BorderFactory.createTitledBorder("Files added"));
-
-        LayoutConstraints.setConstraints(constraints, 0, 0, 1, 2, 1, 100);
-        constraints.fill = GridBagConstraints.BOTH;
-        constraints.anchor = GridBagConstraints.NORTHWEST;
-        gridBag.setConstraints(pnlFilesList, constraints);
-        pnlFilesList.setVisible(true);
-        this.add(pnlFilesList);
-
-        //adiciona a lista de arquivos ao painel esquerdo
-        GridBagLayout gridBagMid = new GridBagLayout();
-        pnlFilesList.setLayout(gridBagMid);
-
-        pnlFiles = new JPanel();
-        JScrollPane jsPane = new JScrollPane(pnlFiles);
-        jsPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        jsPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-        jsPane.setBorder(null);
-
-        constraints = new GridBagConstraints();
-        LayoutConstraints.setConstraints(constraints, 0, 0, 1, 1, 1, 1);
-        constraints.fill = GridBagConstraints.BOTH;
-        constraints.anchor = GridBagConstraints.NORTHWEST;
-        pnlFilesList.add(jsPane, constraints);
-
-        pnlFiles.setLayout(new BoxLayout(pnlFiles, BoxLayout.PAGE_AXIS));
 
         //cria os Comboxes de seleção do documento XML que será mostrado
         leftCB = new JComboBox();
@@ -144,7 +106,6 @@ public class DocumentsTab extends JPanel implements ActionListener {
         this.add(docPane);
 
         //Conclui a construção da interface
-        lineFiles = new ArrayList<LineFile>();
         documents = new Documents();
         this.manager = manager;
     }
@@ -174,34 +135,6 @@ public class DocumentsTab extends JPanel implements ActionListener {
      * @param documents
      */
     public void refresh(Documents documents) {
-        //Atualiza a lista de arquivos a ser exibida
-        final int index = documents.getDocuments().size() - 1;
-        if (index >= 0 && lineFiles.size() <= index) {
-            final LineFile lineFile = new LineFile(documents.getDocument(index + 1));
-
-            lineFile.getButtonUp().addActionListener(new ActionListener() {
-
-                public void actionPerformed(ActionEvent e) {
-                    moveLineFile((LineFile) lineFile.getButtonUp().getParent(), MOVE_UP);
-                }
-            });
-            lineFile.getButtonRemove().addActionListener(new ActionListener() {
-
-                public void actionPerformed(ActionEvent e) {
-                    moveLineFile((LineFile) lineFile.getButtonUp().getParent(), REMOVE);
-                }
-            });
-            lineFile.getButtonDown().addActionListener(new ActionListener() {
-
-                public void actionPerformed(ActionEvent e) {
-                    moveLineFile((LineFile) lineFile.getButtonUp().getParent(), MOVE_DOWN);
-                }
-            });
-
-            pnlFiles.add(lineFile);
-            LineFile.setPnlFiles(pnlFiles);
-            lineFiles.add(lineFile);
-        }
 
         //Variaveis que indicam o indice do XML aberto em cada area de texto
         int leftCBIndex = leftCB.getSelectedIndex();
@@ -245,60 +178,10 @@ public class DocumentsTab extends JPanel implements ActionListener {
         leftCB.addActionListener(this);
         rightCB.addActionListener(this);
 
-        //Redefine a posição dos SplitPane's de acordo com a quantidade de documentos abertos
-        this.docPane.resizeSplitPane(documents.getSize());
-
         //define a nova lista de documentos da classe
         this.documents = documents;
 
         this.revalidate();
-    }
-
-    private void moveLineFile(LineFile caller, int action) {
-        int callerIndex = lineFiles.indexOf(caller);
-        Document currentDoc = documents.getDocuments().get(callerIndex);
-
-        switch (action) {
-            case MOVE_UP:
-                //Troca posições na lista de LineFiles
-                LineFile before = lineFiles.get(callerIndex - 1);
-                lineFiles.set(callerIndex - 1, caller);
-                lineFiles.set(callerIndex, before);
-
-                //Troca posição dos Documents
-                Document beforeDoc = documents.getDocuments().get(callerIndex - 1);
-                documents.getDocuments().set(callerIndex - 1, currentDoc);
-                documents.getDocuments().set(callerIndex, beforeDoc);
-                break;
-            case REMOVE:
-                //Remove da lista de LineFiles
-                lineFiles.remove(callerIndex);
-
-                //Remove dos Documents
-                documents.remove(callerIndex);
-                break;
-            case MOVE_DOWN:
-                //Troca posições na lista de LineFiles
-                LineFile after = lineFiles.get(callerIndex + 1);
-                lineFiles.set(callerIndex + 1, caller);
-                lineFiles.set(callerIndex, after);
-
-                //Troca posição dos Documents                
-                Document afterDoc = documents.getDocuments().get(callerIndex + 1);
-                documents.getDocuments().set(callerIndex + 1, currentDoc);
-                documents.getDocuments().set(callerIndex, afterDoc);
-                break;
-        }
-
-        //Troca posições na UI
-        pnlFiles.removeAll();
-        for (LineFile lf : lineFiles) {
-            pnlFiles.add(lf);
-        }
-
-        //Atualiza UI
-        revalidate();
-        updateUI();
     }
 
     /**
