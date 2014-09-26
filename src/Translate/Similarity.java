c534b6eb168a102ae18099c802b89a08e78f8702
Carlos Oliveira <carlos.rc.oliveira@gmail.com>
2014-09-26 15:29:22 -0300

    Retirada do uso da taxa de similaridade do XChange
    Retirada do uso da taxa de similaridade do XChange, utilizando somente a
    do Phoenix

--- src/Translate/Similarity.java
+++ src/Translate/Similarity.java
@@ -310,7 +310,7 @@ public class Similarity extends ContextKey {
     public boolean similarityCheck(Node node) {
 
         if (node.getNodeType() == Node.ELEMENT_NODE) {
-            NamedNodeMap attributes = (NamedNodeMap) node.getAttributes();
+            /*NamedNodeMap attributes = (NamedNodeMap) node.getAttributes();
             Attr attribute = (Attr) attributes.item(attributes.getLength() - 1);
 
             //Similaridade calculada
@@ -321,7 +321,8 @@ public class Similarity extends ContextKey {
                 return true;
             } else {
                 return false;
-            }
+            }*/
+            return true;
         }
         return false;
     }
